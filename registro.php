d7afd90cd246025a8b60c444778751f0035cc9fb
revenanto <newking.revenant@gmail.com>
2024-05-11 23:47:04 -0400

    Update registro.php

--- registro.php
+++ registro.php
@@ -2,24 +2,26 @@
 require_once 'conexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST["nombre"];
-    $correo = $_POST["correo"];
-    $direccion = $_POST["direccion"];
-    $numero_telefono = $_POST["numero_telefono"];
-    $dueño = $_POST["dueño"] == "Sí"? "Sí" : "No";
-    $tipo_agua = $_POST["tipo_agua"];
-    $fecha = $_POST["fecha"];
+    $nombre_completo = $_POST['nombre_completo'];
+    $correo = $_POST['correo'];
+    $numero_telefono = $_POST['numero_telefono'];
+    $direccion = $_POST['direccion'];
+    $vivienda = $_POST['vivienda'];
+    $tipo_agua = $_POST['tipo_agua'];
+    $menores = $_POST['menores'];
+    $mayores = $_POST['mayores'];
+    $mascota = $_POST['mascota'];
 
     // Verificar si el correo electrónico existe
-    $sql = "SELECT correo FROM captacion WHERE correo = '$correo'";
+    $sql = "SELECT correo FROM peticiones WHERE correo = '$correo'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         echo "El correo electrónico ya existe en la base de datos.";
     } else {
         // Registrar los datos del formulario
-        $sql = "INSERT INTO captacion (nombre, correo, direccion, numero_telefono, dueño, tipo_agua, fecha)
-                VALUES ('$nombre', '$correo', '$direccion', '$numero_telefono', '$dueño', '$tipo_agua', '$fecha')";
+        $sql = "INSERT INTO peticiones (nombre_completo, correo, numero_telefono, direccion, vivienda, tipo_agua, menores, mayores, mascota, fecha) 
+        VALUES ('$nombre_completo', '$correo', '$numero_telefono', '$direccion', '$vivienda', '$tipo_agua', '$menores', '$mayores', '$mascota')";
 
         if ($conn->query($sql) === TRUE) {
             echo "Registro exitoso.";
