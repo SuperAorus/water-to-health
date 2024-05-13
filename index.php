890417f73bc5591e74b3b89dab6a6fb1e9f088c1
Carlos Rodriguez <105572165+SuperAorus@users.noreply.github.com>
2024-05-13 12:25:22 -0600

    mejoras
    mejoras

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de captación</title>
-    <link rel="stylesheet" href="css/style.css">    
+    <link rel="preload" href="css/style.css" as="style">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preload" href="css/normalize.css" as="style">   
     <link rel="stylesheet" href="css/normalize.css">
 </head>
 <body>
@@ -33,7 +38,7 @@
 
 
 <section class="hero">
-    <div>
+     <div>
         <a href="index.php"><img src="img/logo.webp" alt=""></a>
     </div>
 </section>
@@ -41,83 +46,88 @@
 
 
     <div class="formh1">
-        <h1>Formulario de captación</h1>
+        <h1>Form</h1>
     </div>
 
-    <div class="form-container grip-order">
-        <form action="registro.php" method="post">
-            <div class="form-group">
-                <label for="nombre_completo">Nombre completo:</label>
-                <input type="text" name="nombre_completo" id="nombre_completo" required placeholder="Ejemplo: Juan Pérez">
-            </div>
+        <form  class="formulario" action="registro.php" method="post" >
+            <fieldset>
+            <legend> Aplica llenando todos los campos</legend>
+            <div class="contenedor-campos">
+                <div class="form-group">
+                    <label for="nombre_completo">Nombre completo:</label>
+                    <input type="text" name="nombre_completo" id="nombre_completo" required placeholder="Ejemplo: Juan Pérez">
+                </div>
 
-            <div class="form-group">
-                <label for="correo">Correo electrónico:</label>
-                <input type="email" name="correo" id="correo" required placeholder="Ejemplo: juan.perez@example.com">
-            </div>
+                <div class="form-group">
+                    <label for="numero_telefono">Número telefónico:</label>
+                    <input type="tel" name="numero_telefono" id="numero_telefono" required placeholder="Ejemplo: 555-555-5555">
+                </div>
 
-            <div class="form-group">
-                <label for="numero_telefono">Número telefónico:</label>
-                <input type="tel" name="numero_telefono" id="numero_telefono" required placeholder="Ejemplo: 555-555-5555">
-            </div>
+                <div class="form-group">
+                    <label for="correo">Correo electrónico:</label>
+                    <input type="email" name="correo" id="correo" required placeholder="Ejemplo: juan.perez@example.com">
+                </div>
 
-            <div class="form-group">
-                <label for="direccion">Dirección:</label>
-                <input type="text" name="direccion" id="direccion" required placeholder="Ejemplo: Calle 123, Colonia, Ciudad">
-            </div>
 
-            <div class="form-group">
-                <label for="vivienda">Estatus de vivienda:</label>
-                <select name="vivienda" id="vivienda" required>
-                    <option value="selecciona">Selecciona</option>
-                    <option value="1">Vivienda propia</option>
-                    <option value="2">Vivienda de alquiler</option>
-                    <option value="3">Vivienda de familiares</option>
-                </select>
-            </div>
 
-            <div class="form-group">
-                <label for="tipo_agua">Tipo de agua:</label>
-                <select name="tipo_agua" id="tipo_agua" required>
-                    <option value="selecciona">Selecciona</option>
-                    <option value="1">Agua potable</option>
-                    <option value="2">Agua de pozo</option>
-                    <option value="3">Agua de manantial</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label for="direccion">Dirección:</label>
+                    <input type="text" name="direccion" id="direccion" required placeholder="Ejemplo: Calle 123, Colonia, Ciudad">
+                </div>
 
-            <div class="form-group">
-                <label for="menores">Hay menores de edad en el hogar:</label>
-                <select name="menores" id="menores" required>
-                    <option value="selecciona">Selecciona</option>
-                    <option value="1">Si</option>
-                    <option value="2">No</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label for="vivienda">Estatus de vivienda:</label>
+                    <select name="vivienda" id="vivienda" required>
+                        <option value="selecciona">Selecciona</option>
+                        <option value="1">Vivienda propia</option>
+                        <option value="2">Vivienda de alquiler</option>
+                        <option value="3">Vivienda de familiares</option>
+                    </select>
+                </div>
 
-            <div class="form-group">
-                <label for="mayores">Hay mayores de 60 años en el hogar:</label>
-                <select name="mayores" id="mayores" required>
-                    <option value="selecciona">Selecciona</option>
-                    <option value="1">Si</option>
-                    <option value="2">No</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label for="tipo_agua">Tipo de agua:</label>
+                    <select name="tipo_agua" id="tipo_agua" required>
+                        <option value="selecciona">Selecciona</option>
+                        <option value="1">Agua potable</option>
+                        <option value="2">Agua de pozo</option>
+                        <option value="3">Agua de manantial</option>
+                    </select>
+                </div>
 
-            <div class="form-group">
-                <label for="mascota">Tiene mascotas en el hogar:</label>
-                <select name="mascota" id="mascota" required>
-                    <option value="selecciona">Selecciona</option>
-                    <option value="1">Si</option>
-                    <option value="2">No</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label for="menores">Hay menores de edad en el hogar:</label>
+                    <select name="menores" id="menores" required>
+                        <option value="selecciona">Selecciona</option>
+                        <option value="1">Si</option>
+                        <option value="2">No</option>
+                    </select>
+                </div>
 
+                <div class="form-group">
+                    <label for="mayores">Hay mayores de 60 años en el hogar:</label>
+                    <select name="mayores" id="mayores" required>
+                        <option value="selecciona">Selecciona</option>
+                        <option value="1">Si</option>
+                        <option value="2">No</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="mascota">Tiene mascotas en el hogar:</label>
+                    <select name="mascota" id="mascota" required>
+                        <option value="selecciona">Selecciona</option>
+                        <option value="1">Si</option>
+                        <option value="2">No</option>
+                    </select>
+                </div>
+            </div>
             <div class="submit-button">
                 <input type="submit" value="Crear petición">
             </div>
+            </fieldset>
         </form>
-    </div>   
+   
     <script src="script.js"></script>
 </body>
 </html>
