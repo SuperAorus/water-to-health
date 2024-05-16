55480769ea689a362bfa5263b8c6e81fc009f741
Carlos Rodriguez <105572165+SuperAorus@users.noreply.github.com>
2024-05-16 09:35:10 -0600

    big update

--- index.php
+++ index.php
@@ -1,55 +1,129 @@
 <!DOCTYPE html>
-<html lang="es" xml:lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de captación</title>
-    <link rel="preload" href="css/style.css" as="style">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="preload" href="css/normalize.css" as="style">   
+    <link rel="preload" href="css/normalize.css" as="style">
     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preload" href="css/normalize.css" as="style"> 
+    <title>WaterToHealth</title>
+
 </head>
 <body>
+    <!--   HEADER  -->
 
-<!--
-<header class="header">
-    <div class="barra">
-        <a class="logo" href="index.php">
-            <h1 class="logo__nombre no-margin centrar-texto">Water <span class="logo__bold">To Health</span></h1>
-        </a>
+    <header class="header header--transparente  header--scroll" id="header">
+        <div class="barra">
+            <a class="logo" href="#">
+                <h1 class="logo__nombre no-margin centrar-texto">Water <span class="logo__bold">To Health</span></h1>
+                <img src="img/logo.png" alt="logo" class="logo__img">
+              </a>
+        
+            <nav class="navegacion">
+                <button id="btn-menu" class="navegacion__boton">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+        
+                <ul id="menu" class="navegacion__lista">
+                    <li class="navegacion__enlace"><a href="#galeria">Galeria</a></li>
+                    <li class="navegacion__enlace"><a href="#testimonios">Testimonios</a></li>
+                    <li class="navegacion__enlace"><a href="#servicios">Servicios</a></li>
+                    <li class="navegacion__enlace"><a href="#formulario">Contacta con nosotros</a></li>
+                    <li class="navegacion__enlace cerrar-menu"><a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M18 6l-12 12" />
+                        <path d="M6 6l12 12" />
+                      </svg></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
-        <nav class="navegacion">
-            <button id="btn-menu" class="navegacion__boton">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-
-            <ul id="menu" class="navegacion__lista">
-                <li class="navegacion__enlace"><a href="nosotros.html">Nosotros</a></li>
-                <li class="navegacion__enlace"><a href="contacto.html">Contactanos</a></li>
-                <li class="navegacion__enlace"><a href="mostrar_datos.php">Mostrar Datos</a></li>
-                <li class="navegacion__enlace cerrar-menu"><a href="#">Cerrar menú</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
--->
+<!--  Boton whatsapp  -->
 
     <a href="http://wa.me/50671382752" target="blank" class="float">
-        <i class="fa-brands fa-whatsapp"></i>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-whatsapp" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+            <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+          </svg>
     </a>
 
-    <div id="background-video">
-                <video autoplay muted loop>
-                    <source src="video/1.mp4" type="video/mp4">
-                </video>
-    </div>
+<!-- SECTION -->
+<!-- Galeria -->
+<section class="galeria" id="galeria">
+    <div class="borderr">
+        
 
-    <div class="formh1">
-        <a href="index.php"><img src="img/logo.webp" alt=""></a>
+          </div>  
+        <div class="contenedor-imagenes">
+            <img src="img/IMG_8057.jpg" alt="Imagen 1" class="imagen-grande">
+            <img src="img/IMG_8058.jpg" alt="Imagen 2" class="imagen-pequeña">
+            <img src="img/IMG_8059.jpg" alt="Imagen 3" class="imagen-pequeña">
+            <img src="img/IMG_8060.jpg" alt="Imagen 4" class="imagen-pequeña">
+        </div>
     </div>
+</section>
+
+<!--Testimonios-->
+<section class="testimonios". id="testimonios">
+    <div class="padding">
+        <h2>Testimonios</h2>
+        <div class="contenedor-testimonios">
+            <div class="testimonio">
+                <p>"lorem100 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."</p>
+                <cite>- Juan Pérez</cite>
+            </div>
+            <div class="testimonio">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."</p>
+                <cite>- María García</cite>
+            </div>
+            <div class="testimonio">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."</p>
+                <cite>- Pedro López</cite>
+            </div>
+        </div>
+    </div>
+</section>
+
+ <!-- Servicios -->
+<section class="servicios" id="servicios">
+    <div class="contenedor padding">
+        <h2>Nuestros servicios</h2>
+        <div class="contenedor-servicios">
+            <div class="servicio">
+                <img src="img/servicio1.jpg" alt="Servicio 1">
+                <h3>Servicio 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+            </div>
+            <div class="servicio">
+                <img src="img/servicio2.jpg" alt="Servicio 2">
+                <h3>Servicio 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+            </div>
+            <div class="servicio">
+                <img src="img/servicio3.jpg" alt="Servicio 3">
+                <h3>Servicio 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- Form -->
+
+    <section class="wrapper " id="formulario">
+
+        <div id="background-video">
+            <video autoplay muted loop playsinline webkit-playsinline>
+              <source src="video/1.mp4" type="video/mp4">
+            </video>
+          </div>    
 
         <form  class="formulario" action="registro.php" method="post" >
             <fieldset class="pd-sm">
@@ -129,10 +203,28 @@
             </div>
             </fieldset>
         </form>
+    </section>
+
+    <script>
+        const imagenes = document.querySelectorAll('.contenedor-imagenes img');
+        let indice = 0;
     
-        <footer>
-            <p>Water to Health all right reserved</p>
-        </footer>
+        function cambiarImagen() {
+            imagenes.forEach((imagen, i) => {
+                if (i === indice) {
+                    imagen.classList.add('imagen-grande');
+                } else {
+                    imagen.classList.remove('imagen-grande');
+                }
+            });
+    
+            indice = (indice + 1) % imagenes.length;
+        }
+    
+        setInterval(cambiarImagen, 3000);
+    </script>
+
+
     <script src="script.js"></script>
     <script src="https://kit.fontawesome.com/801497830f.js" crossorigin="anonymous"></script>
 </body>
--- script.js
+++ script.js
@@ -11,15 +11,150 @@ document.addEventListener('click', (event) => {
     }
 });
 
+const enlaces = document.querySelectorAll('.navegacion__enlace a');
 
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    if (window.pageYOffset > 0) {
-        header.classList.add('oscuro');
-    } else {
-        header.classList.remove('oscuro');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      enlaces.forEach((enlace) => {
+        enlace.style.color = '#000';
+      });
     }
+  } else {
+    enlaces.forEach((enlace) => {
+      enlace.style.color = '#fff';
+    });
+  }
+});
+
+const logo = document.querySelector('.logo__img');
+const title = document.querySelector('.logo__nombre');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    logo.style.display = 'block';
+    title.style.display = 'none';
+    logo.style.transition = 'all 0.5s ease'; // Agrega una transición suave al mostrar el logotipo
+    title.style.transition = 'all 0.5s ease'; // Agrega una transición suave al ocultar el h1
+  } else {
+    logo.style.display = 'none';
+    title.style.display = 'inline-block';
+  }
 });
 
 
 
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      document.body.style.backgroundColor = '#333';
+    }
+  } else {
+    document.body.style.backgroundColor = '#fff';
+  }
+});
+
+
+const enlacesActivo = document.querySelectorAll('.navegacion__enlace a');
+
+window.addEventListener('scroll', () => {
+  enlacesActivo.forEach((enlace) => {
+    const seccion = document.querySelector(enlace.getAttribute('href'));
+    if (isElementInViewport(seccion)) {
+      enlace.classList.add('activo');
+    } else {
+      enlace.classList.remove('activo');
+    }
+  });
+});
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+
+    const logo = document.querySelector('.logo__img');
+
+logo.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+}
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+      document.querySelector('.header').style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+  } else {
+      document.querySelector('.header').style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+  }
+});
+
+const float = document.querySelector('.float');
+const mensaje = document.createElement('div');
+mensaje.textContent = ' Presiona Si Necesitas Ayuda!';
+mensaje.style.position = 'absolute';
+mensaje.style.left = '-150%';
+mensaje.style.top = '0%';
+mensaje.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
+mensaje.style.padding = '10px';
+mensaje.style.borderRadius = '5px';
+mensaje.style.color = 'black ';
+mensaje.style.transition = 'all 1s ease';
+
+
+
+setTimeout(() => {
+  float.appendChild(mensaje);
+    mensaje.style.left = '-230%';
+}, 500);
+
+setTimeout(() => {
+  mensaje.style.left = '300%';
+}, 4000);
+
+float.addEventListener('mouseover', () => {
+  mensaje.style.left = '-230%';
+});
+
+float.addEventListener('mouseout', () => {
+  mensaje.style.left = '300%';
+});
+
+const whatsappButton = document.querySelector('.float');
+
+let isScrolling = false;
+
+window.addEventListener('scroll', () => {
+  isScrolling = true;
+  whatsappButton.classList.add('scrolling');
+});
+
+window.addEventListener('scrollend', () => {
+  isScrolling = false;
+  whatsappButton.classList.remove('scrolling');
+});
+
+setInterval(() => {
+  if (!isScrolling) {
+    whatsappButton.classList.remove('scrolling');
+  }
+}, 100);
+
+
+let scrollTimeout;
+
+window.addEventListener('scroll', () => {
+  isScrolling = true;
+  whatsappButton.classList.add('scrolling');
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(() => {
+    isScrolling = false;
+    whatsappButton.classList.remove('scrolling');
+  }, 200);
+});
